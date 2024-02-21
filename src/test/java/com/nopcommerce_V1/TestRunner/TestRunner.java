7bb091d0abac6e600e04acaa65df192acad702cb
surya <ksurya2008@gmail.com>
2024-02-20 21:46:30 -0500

    changed testrunner

--- src/test/java/com/nopcommerce_V1/TestRunner/TestRunner.java
+++ src/test/java/com/nopcommerce_V1/TestRunner/TestRunner.java
@@ -1,5 +1,17 @@
 package com.nopcommerce_V1.TestRunner;
 
-public class TestRunner {
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-}
+@CucumberOptions(features = {"src/test/resources/Features"},
+glue = {"StepDefinitions", "Hooks"})
+
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+	   
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+    return super.scenarios();
+  }
+}  
