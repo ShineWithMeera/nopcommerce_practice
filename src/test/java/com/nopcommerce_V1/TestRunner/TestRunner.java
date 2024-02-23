c3e156b74ef6b65030e4bceead6bf693761a6cd0
Pragyan M <Pragyan0304@gmail.com>
2024-02-23 11:37:34 -0500

    testdata added

--- src/test/java/com/nopcommerce_V1/TestRunner/TestRunner.java
+++ src/test/java/com/nopcommerce_V1/TestRunner/TestRunner.java
@@ -1,5 +1,24 @@
 package com.nopcommerce_V1.TestRunner;
 
-public class TestRunner {
+import org.testng.annotations.Test;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@Test
+@CucumberOptions(
+		
+		features= "/src/test/resources/Features",
+		glue = "StepDefinitions"
+		
+		
+		)
+
+
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+	
+	
+	
 }
