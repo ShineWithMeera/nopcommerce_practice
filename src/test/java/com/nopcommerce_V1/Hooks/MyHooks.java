2626d9d53f50974e2cd5cb04c271bdb0d7e3a070
Pragyan M <Pragyan0304@gmail.com>
2024-03-11 13:21:09 -0400

    hooks

--- src/test/java/com/nopcommerce_V1/Hooks/MyHooks.java
+++ src/test/java/com/nopcommerce_V1/Hooks/MyHooks.java
@@ -1,20 +1,44 @@
 package com.nopcommerce_V1.Hooks;
 
+import org.openqa.selenium.WebDriver;
+
+import com.nopcommerce_V1.DriverFactory.DriverFactory;
+import com.nopcommerce_V1.Utilities.ConfigReader;
+import com.nopcommerce_V1.Utilities.LoggerLoad;
+
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 
 
 
 public class MyHooks {
 
-	
+	   public static DriverFactory driverfactory;
+	   public static WebDriver driver;
 	
 	@BeforeAll
 	public static void before() throws Throwable {
 		
-		
+		//Get browser Type from config file
+		LoggerLoad.info("Loading config file");
+	    ConfigReader.loadConfig();
 
+		 String browser=  ConfigReader.getBrowserType();
+
+	    
+	  //Initialize driver from driver factory
+	    driverfactory = new DriverFactory ();
+		driver = driverfactory.initializeDrivers(browser);
+	    LoggerLoad.info("Initializing driver for : "+browser);
+	    
 	}
 	
+	@AfterAll
+	
+	public static void after() {
+		LoggerLoad.info("Closing Driver");
+	    
+	}	
 	
 	
 	
